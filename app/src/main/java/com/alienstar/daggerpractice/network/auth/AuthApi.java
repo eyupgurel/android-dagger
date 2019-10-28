6baeb6fb16626fb9c109aa5c193b6be52562ea7a
Eyup Gurel <eyupgurel@gmail.com>
2019-10-28 23:42:54 +0300

    RXJava mode active

--- app/src/main/java/com/alienstar/daggerpractice/network/auth/AuthApi.java
+++ app/src/main/java/com/alienstar/daggerpractice/network/auth/AuthApi.java
@@ -1,10 +1,14 @@
 package com.alienstar.daggerpractice.network.auth;
 
+import com.alienstar.daggerpractice.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User>  getUser(@Path("id") int id);
 }
--- app/src/main/java/com/alienstar/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/alienstar/daggerpractice/ui/auth/AuthViewModel.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.alienstar.daggerpractice.models.User;
 import com.alienstar.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
@@ -15,5 +20,30 @@ public class AuthViewModel extends ViewModel {
     public AuthViewModel(AuthApi authApi) {
         this.authApi=authApi;
         Log.d(TAG, "AuthViewModel:  viewmodel is working");
+        authApi.getUser(1).toObservable().
+                subscribeOn(Schedulers.io()).
+                subscribe(new Observer<User>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(User user) {
+                Log.d(TAG, "onNext: " + user.getEmail());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: ", e);
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
     }
 }
